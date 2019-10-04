6a23dc43cb2b8cfe864bee6041f82e9f3ec99e17
JazzyMussels <jeffmussels@gmail.com>
2019-10-04 14:21:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
+
+function superbowlWin(rec) {
+ let result = rec.find(o => o.result === 'W');
+ return result ? result.year : undefined
 }
